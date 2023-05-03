9c242ee65f8d5f6cce29a2cb4773adcf7347c23d
Giyyu <santivrgs@gmail.com>
2023-05-03 08:16:16 -0500

    eliminados componentes auth - preparando para organizacion correcta

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './components/login/login.component';
-import { SigninComponent } from './components/signin/signin.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    SigninComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
